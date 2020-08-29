1ae87666b96a77639ee30afe470b78373307e249
nekunei <68283366+nekunei@users.noreply.github.com>
2020-08-29 08:12:43 +0700

    Update uptimer.js

--- uptimer.js
+++ uptimer.js
@@ -3,7 +3,7 @@ const http = require("http");
 const request = require("request");
 function pingURL(url) {
  return new Promise((resp, rej) => {
-   request(url,{ headers: { 'User-Agent': `WatchURL-${Math.floor(Math.random() * (99999999999999999 - 1)) + 1}` } }, (err, res, body) => {
+   request(url,{ headers: { 'User-Agent': `${Math.floor(Math.random() * (99999999999999999 - 1)) + 1}` } }, (err, res, body) => {
      if (err) {rej()} else {resp();}
      
   
@@ -14,26 +14,7 @@ function pingURL(url) {
 function ping()
 {
   if (!process.env.DOMAIN) return;
-  
- // console.log("---------Loading...---------");
-  
- //fs.readFile("./urls-save.json", "utf8", function(err, contents) {
-    
-      //try {
-    //    let j = JSON.parse(contents);
-        
-        
-          pingURL(DOMAIN);
-        /*  console.log(`Sucessfully pinged ${url}!`);
-          
-            console.log(`Unable to ping ${url}! Status code: NuLl.`);
-       */
-       
-  /*    } catch(e) {
-        console.log("Unable to parse!")
-        console.log(err);
-      } */
-  
+pingURL(DOMAIN);
     }
 setInterval(() => {
  ping();
